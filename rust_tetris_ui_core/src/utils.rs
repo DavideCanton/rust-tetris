e4d3771648fe18de2788e1d3f5fcbdd1a748b380
Davide Canton <davide.canton5@gmail.com>
2020-11-08 02:17:06 +0100

    added unit tests

--- rust_tetris_ui_core/src/utils.rs
+++ rust_tetris_ui_core/src/utils.rs
@@ -14,6 +14,7 @@ pub const WIN_W: f32 = 800.0;
 pub const WIN_H: f32 = 600.0;
 pub const BASE_X: f32 = (WIN_W - (WIDTH * 10.0f32)) / 2.0;
 pub const HOLD_X: f32 = (BASE_X - (WIDTH * 3.0f32)) / 2.0;
+pub const GHOST_ALPHA: f32 = 0.3;
 
 pub const BLACK: Color = Color::new(0.0, 0.0, 0.0, 1.0);
 pub const WHITE: Color = Color::new(1.0, 1.0, 1.0, 1.0);
@@ -40,7 +41,7 @@ fn apply_shadow(original_color: Color, is_shadow: bool) -> Color {
     let mut color = original_color;
 
     if is_shadow {
-        color.a = 0.3;
+        color.a = GHOST_ALPHA;
     }
 
     color
@@ -72,4 +73,112 @@ pub fn is_not_empty(kick: Kick) -> bool {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::utils::*;
+    use rust_tetris_core::pieces::PlayableTetrisPieceType;
+
+    #[test]
+    fn test_is_not_empty() {
+        let mut kick = (1, 0);
+        assert_eq!(is_not_empty(kick), true);
+
+        kick = (0, 1);
+        assert_eq!(is_not_empty(kick), true);
+
+        kick = (1, 1);
+        assert_eq!(is_not_empty(kick), true);
+
+        kick = (0, 0);
+        assert_eq!(is_not_empty(kick), false);
+    }
+
+    #[test]
+    fn test_playable_piece_to_color_no_shadow() {
+        let pairs = vec![
+            (PlayableTetrisPieceType::I, I_COLOR),
+            (PlayableTetrisPieceType::O, O_COLOR),
+            (PlayableTetrisPieceType::S, S_COLOR),
+            (PlayableTetrisPieceType::Z, Z_COLOR),
+            (PlayableTetrisPieceType::L, L_COLOR),
+            (PlayableTetrisPieceType::J, J_COLOR),
+            (PlayableTetrisPieceType::T, T_COLOR),
+        ];
+
+        for (piece, expected_color) in pairs {
+            let color = playable_piece_to_color(piece, false);
+            assert_eq!(color, expected_color, "For piece {:?} color should be {:?}, got {:?}", piece, expected_color, color);
+        }
+    }
+
+    #[test]
+    fn test_playable_piece_to_color_with_shadow() {
+        let mut pairs = vec![
+            (PlayableTetrisPieceType::I, I_COLOR),
+            (PlayableTetrisPieceType::O, O_COLOR),
+            (PlayableTetrisPieceType::S, S_COLOR),
+            (PlayableTetrisPieceType::Z, Z_COLOR),
+            (PlayableTetrisPieceType::L, L_COLOR),
+            (PlayableTetrisPieceType::J, J_COLOR),
+            (PlayableTetrisPieceType::T, T_COLOR),
+        ];
+
+        for (_, c) in pairs.iter_mut() {
+            c.a = GHOST_ALPHA;
+        }
+
+        for (piece, expected_color) in pairs {
+            let color = playable_piece_to_color(piece, true);
+            assert_eq!(color, expected_color, "For piece {:?} color should be {:?}, got {:?}", piece, expected_color, color);
+        }
+    }
+
+    #[test]
+    fn test_piece_to_color_no_shadow() {
+        let pairs = vec![
+            (PlayableTetrisPieceType::I, I_COLOR),
+            (PlayableTetrisPieceType::O, O_COLOR),
+            (PlayableTetrisPieceType::S, S_COLOR),
+            (PlayableTetrisPieceType::Z, Z_COLOR),
+            (PlayableTetrisPieceType::L, L_COLOR),
+            (PlayableTetrisPieceType::J, J_COLOR),
+            (PlayableTetrisPieceType::T, T_COLOR),
+        ];
+
+        for (piece, expected_color) in pairs {
+            let piece_type = TetrisPieceType::Playable(piece);
+            let color = piece_to_color(piece_type, false);
+            assert_eq!(color, expected_color, "For piece {:?} color should be {:?}, got {:?}", piece_type, expected_color, color);
+        }
+
+        let color = piece_to_color(TetrisPieceType::NotPlayable, false);
+        assert_eq!(color, OTHER_COLOR, "For piece {:?} color should be {:?}, got {:?}", TetrisPieceType::NotPlayable, OTHER_COLOR, color);
+    }
+
+    #[test]
+    fn test_piece_to_color_with_shadow() {
+        let mut pairs = vec![
+            (PlayableTetrisPieceType::I, I_COLOR),
+            (PlayableTetrisPieceType::O, O_COLOR),
+            (PlayableTetrisPieceType::S, S_COLOR),
+            (PlayableTetrisPieceType::Z, Z_COLOR),
+            (PlayableTetrisPieceType::L, L_COLOR),
+            (PlayableTetrisPieceType::J, J_COLOR),
+            (PlayableTetrisPieceType::T, T_COLOR),
+        ];
+
+        for (_, c) in pairs.iter_mut() {
+            c.a = GHOST_ALPHA;
+        }
+
+        for (piece, expected_color) in pairs {
+            let piece_type = TetrisPieceType::Playable(piece);
+            let color = piece_to_color(piece_type, true);
+            assert_eq!(color, expected_color, "For piece {:?} color should be {:?}, got {:?}", piece_type, expected_color, color);
+        }
+
+        let color = piece_to_color(TetrisPieceType::NotPlayable, true);
+        let mut expected_color = OTHER_COLOR.clone();
+        expected_color.a = GHOST_ALPHA;
+        assert_eq!(color, expected_color, "For piece {:?} color should be {:?}, got {:?}", TetrisPieceType::NotPlayable, expected_color, color);
+    }
+}
